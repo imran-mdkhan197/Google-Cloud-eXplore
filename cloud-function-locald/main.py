4c6ff4b125b1e1eadf4ada6b2d920e23e98b48f6
Imran Khan <mikhan.osv@fedex.com>
2021-05-15 09:50:53 +0530

    Initial Commit

---
+++ cloud-function-locald/main.py
@@ -0,0 +1,12 @@
+
+from mypythonlib import myfunctions
+
+def hello_gcs(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    file = event
+    print(f"Processing file: {file['name']}.")
+    print(myfunctions.custom_calc(5,5))
---
+++ cloud-function-locald/mypythonlib/myfunctions.py
@@ -0,0 +1,2 @@
+def custom_calc(a, b):
+	return a*a+b
